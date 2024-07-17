b053f71d719ed2c6c60126cbbc83e622098ff7e4
gjl <2802427218@qq.com>
2024-07-17 17:48:56 +0800

    feat: 新增 OpenAI 兼容接口路由

--- src/mindpilot/app/utils.py
+++ src/mindpilot/app/utils.py
@@ -142,9 +142,10 @@ def get_ChatOpenAI(
         #         openai_api_key="",
         #         openai_proxy="",
         #     )
+        # TODO 配置文件
         params.update(
-            openai_api_base="",
-            openai_api_key="",
+            openai_api_base="https://open.bigmodel.cn/api/paas/v4/",
+            openai_api_key="8424573178d3681bb2e9bfbc5af24dd5.BKKxdk1d6zzgvfnV",
             openai_proxy="",
         )
         model = ChatOpenAI(**params)
@@ -170,11 +171,11 @@ def get_prompt_template(type: str, name: str) -> Optional[str]:
 def get_tool(name: str = None) -> Union[BaseTool, Dict[str, BaseTool]]:
     import importlib
 
-    from ..app import tools
+    from app import tools
 
     importlib.reload(tools)
 
-    from ..app.tools import tools_registry
+    from app.tools import tools_registry
 
     # update_search_local_knowledgebase_tool()
 
@@ -197,3 +198,131 @@ async def wrap_done(fn: Awaitable, event: asyncio.Event):
     finally:
         # Signal the aiter to stop.
         event.set()
+
+def get_OpenAIClient(
+    platform_name: str = None,
+    model_name: str = None,
+    is_async: bool = True,
+) -> Union[openai.Client, openai.AsyncClient]:
+    # """
+    # construct an openai Client for specified platform or model
+    # """
+    # if platform_name is None:
+    #     platform_info = get_model_info(
+    #         model_name=model_name, platform_name=platform_name
+    #     )
+    #     if platform_info is None:
+    #         raise RuntimeError(
+    #             f"cannot find configured platform for model: {model_name}"
+    #         )
+    #     platform_name = platform_info.get("platform_name")
+    # platform_info = get_config_platforms().get(platform_name)
+    # assert platform_info, f"cannot find configured platform: {platform_name}"
+    # TODO 配置文件
+    params = {
+        "base_url":"https://open.bigmodel.cn/api/paas/v4/",
+        "api_key": "8424573178d3681bb2e9bfbc5af24dd5.BKKxdk1d6zzgvfnV"
+    }
+    httpx_params = {}
+    # if api_proxy := platform_info.get("api_proxy"):
+    #     httpx_params = {
+    #         "proxies": api_proxy,
+    #         "transport": httpx.HTTPTransport(local_address="0.0.0.0"),
+    #     }
+
+    if is_async:
+        if httpx_params:
+            params["http_client"] = httpx.AsyncClient(**httpx_params)
+        return openai.AsyncClient(**params)
+    else:
+        if httpx_params:
+            params["http_client"] = httpx.Client(**httpx_params)
+        return openai.Client(**params)
+
+def get_tool_config(name: str = None) -> Dict:
+    import importlib
+
+    # from chatchat.configs import model_config
+    # importlib.reload(model_config)
+    # from chatchat.configs import TOOL_CONFIG
+    TOOL_CONFIG = {
+        # "search_local_knowledgebase": {
+        #     "use": False,
+        #     "top_k": 3,
+        #     "score_threshold": 1.0,
+        #     "conclude_prompt": {
+        #         "with_result": '<指令>根据已知信息，简洁和专业的来回答问题。如果无法从中得到答案，请说 "根据已知信息无法回答该问题"，'
+        #                        "不允许在答案中添加编造成分，答案请使用中文。 </指令>\n"
+        #                        "<已知信息>{{ context }}</已知信息>\n"
+        #                        "<问题>{{ question }}</问题>\n",
+        #         "without_result": "请你根据我的提问回答我的问题:\n"
+        #                           "{{ question }}\n"
+        #                           "请注意，你必须在回答结束后强调，你的回答是根据你的经验回答而不是参考资料回答的。\n",
+        #     },
+        # },
+        "search_internet": {
+            "use": False,
+            "search_engine_name": "bing",
+            "search_engine_config": {
+                "bing": {
+                    "result_len": 3,
+                    "bing_search_url": "https://api.bing.microsoft.com/v7.0/search",
+                    "bing_key": "0f42b09dce16474a81c01562ded071dc",
+                },
+                "metaphor": {
+                    "result_len": 3,
+                    "metaphor_api_key": "",
+                    "split_result": False,
+                    "chunk_size": 500,
+                    "chunk_overlap": 0,
+                },
+                "duckduckgo": {"result_len": 3},
+            },
+            "top_k": 10,
+            "verbose": "Origin",
+            "conclude_prompt": "<指令>这是搜索到的互联网信息，请你根据这些信息进行提取并有调理，简洁的回答问题。如果无法从中得到答案，请说 “无法搜索到能回答问题的内容”。 "
+                               "</指令>\n<已知信息>{{ context }}</已知信息>\n"
+                               "<问题>\n"
+                               "{{ question }}\n"
+                               "</问题>\n",
+        },
+        "arxiv": {
+            "use": False,
+        },
+        "shell": {
+            "use": False,
+        },
+        "weather_check": {
+            "use": False,
+            "api_key": "SE7CGiRD5dvls08Ub",
+        },
+        # "search_youtube": {
+        #     "use": False,
+        # },
+        "wolfram": {
+            "use": False,
+            "appid": "PWKVLW-6ETR93QX6Q",
+        },
+        "calculate": {
+            "use": False,
+        },
+        # "vqa_processor": {
+        #     "use": False,
+        #     "model_path": "your model path",
+        #     "tokenizer_path": "your tokenizer path",
+        #     "device": "cuda:1",
+        # },
+        # "aqa_processor": {
+        #     "use": False,
+        #     "model_path": "your model path",
+        #     "tokenizer_path": "yout tokenizer path",
+        #     "device": "cuda:2",
+        # },
+        # "text2images": {
+        #     "use": False,
+        # },
+    }
+    if name is None:
+        return TOOL_CONFIG
+    else:
+        return TOOL_CONFIG.get(name, {})
