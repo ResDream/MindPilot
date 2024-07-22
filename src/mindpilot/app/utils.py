760555d7eb657035eab3fed9e3f2428fc462115b
gjl <2802427218@qq.com>
2024-07-22 16:56:49 +0800

    V0.1.0

--- src/mindpilot/app/utils.py
+++ src/mindpilot/app/utils.py
@@ -117,7 +117,6 @@ def get_ChatOpenAI(
         local_wrap: bool = False,  # use local wrapped api
         **kwargs: Any,
 ) -> ChatOpenAI:
-    # model_info = get_model_info(model_name)
     params = dict(
         streaming=streaming,
         verbose=verbose,
@@ -128,20 +127,6 @@ def get_ChatOpenAI(
         **kwargs,
     )
     try:
-        # if local_wrap:
-        #     params.update(
-        #         openai_api_base=f"{api_address()}/v1",
-        #         openai_api_key="EMPTY",
-        #     )
-        # else:
-        #     params.update(
-        #         # openai_api_base=model_info.get("api_base_url"),
-        #         # openai_api_key=model_info.get("api_key"),
-        #         # openai_proxy=model_info.get("api_proxy"),
-        #         openai_api_base="",
-        #         openai_api_key="",
-        #         openai_proxy="",
-        #     )
         # TODO 配置文件
         params.update(
             openai_api_base="https://open.bigmodel.cn/api/paas/v4/",
@@ -177,7 +162,6 @@ def get_tool(name: str = None) -> Union[BaseTool, Dict[str, BaseTool]]:
 
     from app.tools import tools_registry
 
-    # update_search_local_knowledgebase_tool()
 
     if name is None:
         return tools_registry._TOOLS_REGISTRY
@@ -240,88 +224,9 @@ def get_OpenAIClient(
         return openai.Client(**params)
 
 def get_tool_config(name: str = None) -> Dict:
-    import importlib
 
-    # from chatchat.configs import model_config
-    # importlib.reload(model_config)
-    # from chatchat.configs import TOOL_CONFIG
-    TOOL_CONFIG = {
-        # "search_local_knowledgebase": {
-        #     "use": False,
-        #     "top_k": 3,
-        #     "score_threshold": 1.0,
-        #     "conclude_prompt": {
-        #         "with_result": '<指令>根据已知信息，简洁和专业的来回答问题。如果无法从中得到答案，请说 "根据已知信息无法回答该问题"，'
-        #                        "不允许在答案中添加编造成分，答案请使用中文。 </指令>\n"
-        #                        "<已知信息>{{ context }}</已知信息>\n"
-        #                        "<问题>{{ question }}</问题>\n",
-        #         "without_result": "请你根据我的提问回答我的问题:\n"
-        #                           "{{ question }}\n"
-        #                           "请注意，你必须在回答结束后强调，你的回答是根据你的经验回答而不是参考资料回答的。\n",
-        #     },
-        # },
-        "search_internet": {
-            "use": False,
-            "search_engine_name": "bing",
-            "search_engine_config": {
-                "bing": {
-                    "result_len": 3,
-                    "bing_search_url": "https://api.bing.microsoft.com/v7.0/search",
-                    "bing_key": "0f42b09dce16474a81c01562ded071dc",
-                },
-                "metaphor": {
-                    "result_len": 3,
-                    "metaphor_api_key": "",
-                    "split_result": False,
-                    "chunk_size": 500,
-                    "chunk_overlap": 0,
-                },
-                "duckduckgo": {"result_len": 3},
-            },
-            "top_k": 10,
-            "verbose": "Origin",
-            "conclude_prompt": "<指令>这是搜索到的互联网信息，请你根据这些信息进行提取并有调理，简洁的回答问题。如果无法从中得到答案，请说 “无法搜索到能回答问题的内容”。 "
-                               "</指令>\n<已知信息>{{ context }}</已知信息>\n"
-                               "<问题>\n"
-                               "{{ question }}\n"
-                               "</问题>\n",
-        },
-        "arxiv": {
-            "use": False,
-        },
-        "shell": {
-            "use": False,
-        },
-        "weather_check": {
-            "use": False,
-            "api_key": "SE7CGiRD5dvls08Ub",
-        },
-        # "search_youtube": {
-        #     "use": False,
-        # },
-        "wolfram": {
-            "use": False,
-            "appid": "PWKVLW-6ETR93QX6Q",
-        },
-        "calculate": {
-            "use": False,
-        },
-        # "vqa_processor": {
-        #     "use": False,
-        #     "model_path": "your model path",
-        #     "tokenizer_path": "your tokenizer path",
-        #     "device": "cuda:1",
-        # },
-        # "aqa_processor": {
-        #     "use": False,
-        #     "model_path": "your model path",
-        #     "tokenizer_path": "yout tokenizer path",
-        #     "device": "cuda:2",
-        # },
-        # "text2images": {
-        #     "use": False,
-        # },
-    }
+    from app.configs import TOOL_CONFIG
+
     if name is None:
         return TOOL_CONFIG
     else:
