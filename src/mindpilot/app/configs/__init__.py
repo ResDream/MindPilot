a0755528f34ef12a0457ad2c4740aff9884f3135
gjl <2802427218@qq.com>
2024-08-13 21:12:43 +0800

    fix:解决agent_enable为false无法输出的bug

--- src/mindpilot/app/configs/__init__.py
+++ src/mindpilot/app/configs/__init__.py
@@ -1,6 +1,6 @@
 from .system_config import HOST, PORT
 from .model_config import MODEL_CONFIG
-from .prompt_config import OPENAI_PROMPT
+from .prompt_config import OPENAI_PROMPT, PROMPT_TEMPLATES
 from .tool_config import TOOL_CONFIG
 
 __all__ = [
@@ -9,4 +9,5 @@ __all__ = [
     "MODEL_CONFIG",
     "OPENAI_PROMPT",
     "TOOL_CONFIG",
+    "PROMPT_TEMPLATES"
 ]
