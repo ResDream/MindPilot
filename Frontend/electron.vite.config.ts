96264ffa3e19a7c385edafdb9866a9e02cf3f65e
fanxing <fanxingin@outlook.com>
2024-10-18 20:49:40 +0800

    不包含环境检查/安装的版本

--- Frontend/electron.vite.config.ts
+++ Frontend/electron.vite.config.ts
@@ -23,14 +23,6 @@ export default defineConfig({
           multipass: true
         }
       })
-    ],
-    build: {
-      rollupOptions: {
-        input: {
-          index: resolve(__dirname, 'src/renderer/index.html'),
-          envCheck: resolve(__dirname, 'src/renderer/env-check.html')
-        }
-      }
-    }
+    ]
   }
 });
--- Frontend/src/renderer/src/env-check.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import EnvCheck from './views/EnvCheck.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-const app = createApp(EnvCheck)
-app.use(ElementPlus)
-app.mount('#app')
